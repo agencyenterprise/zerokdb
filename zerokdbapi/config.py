129cfa027718174358b363198f808afb4e6ee4b0
kevinkons <kevin.kons@hotmail.com>
2024-10-04 11:54:28 -0300

    fixing Settings on zerokdbapi

--- zerokdbapi/config.py
+++ zerokdbapi/config.py
@@ -4,11 +4,6 @@ from dotenv import find_dotenv
 
 class Settings(BaseSettings):
     pinata_api_key: str
-    provider_url: str
-    contract_address: str
-    abi_path: str
-    from_address: str
-    private_key: str
     api_host: str
     aptos_table_sequence_contract: str
     aptos_private_key: str
